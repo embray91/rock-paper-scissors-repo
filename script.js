6647ef2c10134cb18bd657d94ca45977b5d6597c
embray91 <embray91@gmail.com>
2021-11-23 21:56:00 -0600

    added ui to rps game

--- script.js
+++ script.js
@@ -10,42 +10,68 @@ function computerPlay() {
     } else {
         return scissors;
     }
-}
+};
+
 const win = "You win!";
 const lose = "You lose!";
 const draw = "Draw!";
 
+let playerScore = 0;
+let compScore=0;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
-        return win;
+        result.textContent=win;
+        playerScore=playerScore+1;
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
-        return lose;
+        result.textContent=lose;
+        compScore=compScore+1;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
-        return win;
+        result.textContent=win;
+          playerScore=playerScore+1;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
-        return lose;
+        result.textContent=lose;
+        compScore=compScore+1;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
-        return win;
+        result.textContent=win;
+           playerScore=playerScore+1;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
-        return lose;
+        result.textContent=lose;
+        compScore=compScore+1;
     } else if (playerSelection.toLowerCase() == computerSelection) {
-        return draw;
-    }
+        result.textContent=draw;
+    } 
 }
 
-function game() {
-    let compScore=0;
-    let playerScore=0;
-    for (let i=0; i<=5; i++) {
-    let playerSelection = prompt("Choose your weapon.");
-    let computerSelection = computerPlay();
-    let result = playRound(playerSelection, computerSelection);
-    if (result == win) {
-    playerScore++;
-    } else if (result == lose) {
-    compScore++;
-    } else if (result == draw) {
-        continue;
-    }
-    } alert(`The final score for the computer is ${compScore} and the final score for you is ${playerScore} !`);
+
+
+const container = document.querySelector('#container');
+const result = document.querySelector('#result');
+let p = document.querySelector('#num');
+
+
+const choose = (e) => {
+    const choice = e.currentTarget.id; 
+    const compPlay = computerPlay();
+    playRound(choice, compPlay);
+    p.textContent= `The player's score is ${playerScore} and the computer's score is ${compScore}.`
+    result.appendChild(p);
+if (playerScore === 5) {
+    alert(`You are the winner!`)
+    playerScore=0;
+    compScore=0;
+    p.textContent= `The player's score is ${playerScore} and the computer's score is ${compScore}.`
+} else if (compScore ===5) {
+    alert(`The computer wins!`);
+    playerScore=0;
+    compScore=0;
+    p.textContent= `The player's score is ${playerScore} and the computer's score is ${compScore}.`
 }
+};
+
+document.querySelector('#rock').addEventListener('click', choose);
+document.querySelector('#paper').addEventListener('click', choose);
+document.querySelector('#scissors').addEventListener('click', choose);
+
+container.appendChild(result);
+
